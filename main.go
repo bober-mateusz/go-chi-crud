5b2689c4c41e4f1b566b79c728290e934465d478
Matt Bober <bober.mateusz17@Gmail.com>
2023-11-21 14:06:21 +0000

    Quick Commit

--- main.go
+++ main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/bober-mateusz/go-chi-crud/application"
 )
 
 func main() {
-	router := chi.NewRouter()
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-	router.Use(middleware.Logger)
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app: ", err)
 	}
-
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
 }
